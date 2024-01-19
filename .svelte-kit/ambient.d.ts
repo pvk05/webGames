78c9cdf07e26c310d3f6a0a37241dc34a6c2cf47
pvk05 <philip.v.knudsen@gmail.com>
2024-01-19 09:52:11 +0100

    Delete unused vercel files and configurations

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -39,9 +39,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_27988: string;
-	export const FPS_BROWSER_APP_PROFILE_STRING: string;
-	export const FPS_BROWSER_USER_PROFILE_STRING: string;
+	export const EFC_10104: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -154,9 +152,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_27988: string;
-		FPS_BROWSER_APP_PROFILE_STRING: string;
-		FPS_BROWSER_USER_PROFILE_STRING: string;
+		EFC_10104: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/6.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 6;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/clickerGame/info/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/6.7229dc8d.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.a21d6cee.js","_app/immutable/chunks/nav.5a67d195.js"];
-export const stylesheets = ["_app/immutable/assets/6.e1528987.css","_app/immutable/assets/nav.4cc18e73.css"];
-export const fonts = [];

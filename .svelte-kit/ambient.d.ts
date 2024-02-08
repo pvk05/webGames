692fb1fda126eb03b099441cf900d3df241980ec
pvk05 <philip.v.knudsen@gmail.com>
2024-02-08 22:16:14 +0100

    Update environment variables and version hash

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -39,7 +39,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_10104: string;
+	export const EFC_11636: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -152,7 +152,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_10104: string;
+		EFC_11636: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;

3101aec1424604f4bdc1449e768c79a58015925c
pvk05 <philip.v.knudsen@gmail.com>
2024-08-19 00:52:39 +0200

    tested subscribe functionality

--- src/routes/websocket/+page.svelte
+++ src/routes/websocket/+page.svelte
@@ -1,13 +1,14 @@
 <script>
-    import { onMount } from 'svelte';
+    import { onMount } from "svelte";
+    let ws;
+    let msg = "";
 
     let messages = [];
     onMount(() => {
-        const ws = new WebSocket('ws://localhost:8080');
+        ws = new WebSocket("ws://localhost:8080");
 
         ws.onopen = () => {
-            console.log('connected');
-            ws.send('Hello Server!');
+            console.log("connected");
         };
 
         ws.onmessage = (event) => {
@@ -16,13 +17,57 @@
         };
 
         ws.onclose = () => {
-            console.log('disconnected');
+            console.log("disconnected");
+        };
+    });
+
+    function sendMsg() {
+        let data = {
+            type: "chatMsg",
+            channel: "chat",
+            message: msg,
         };
 
-    });
+        ws.send(data);
+    }
+
+    function subscribeToWs(topic) {
+        ws.send({
+            type: "subscribe",
+            topicName: topic,
+        });
+    }
 </script>
 
 <h1 class=" h1">Websocket</h1>
+
+<h3>
+    {#if ws}
+        Connected
+    {:else}
+        Not Connected
+    {/if}
+</h3>
+<br />
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("ludo")}
+>
+    Sub to 'Ludo'
+</button>
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("yatzy")}>Sub to 'yatzy'</button
+>
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("gemgo")}>Sub to 'gemgo'</button
+>
+
+<form on:submit={sendMsg}>
+    <input type="text" name="msgInp" id="msgInp" bind:value={msg} />
+    <input type="submit" value="Send" />
+</form>
 
 <div id="messages">
     {#each messages as message}

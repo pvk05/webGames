55cffc2c400e7b0c2e274e55b2d6c298f64500a8
pvk05 <philip.v.knudsen@gmail.com>
2024-08-22 23:17:21 +0200

    added username input to websocket page

--- src/routes/websocket/+page.svelte
+++ src/routes/websocket/+page.svelte
@@ -1,78 +1,15 @@
 <script>
-    import { onMount } from "svelte";
-    let ws;
-    let msg = "";
+    import { writable, get } from "svelte/store";
+    import Chat from "./chat.svelte";
 
-    let messages = [];
-    onMount(() => {
-        ws = new WebSocket("ws://localhost:8080");
-
-        ws.onopen = () => {
-            console.log("connected");
-        };
-
-        ws.onmessage = (event) => {
-            console.log(event.data);
-            messages = [...messages, event.data];
-        };
-
-        ws.onclose = () => {
-            console.log("disconnected");
-        };
-    });
-
-    function sendMsg() {
-        let data = JSON.stringify({
-            type: "chatMsg",
-            channel: "ludo",
-            message: msg,
-        });
-
-        ws.send(data);
-    }
-
-    function subscribeToWs(topic) {
-        let data = JSON.stringify({
-            type: "subscribe",
-            topicName: topic,
-        })
-
-        ws.send(data);
-    }
+    let loggedIn = writable(false)
+    console.log(get(loggedIn))
+    let username = ""
 </script>
 
-<h1 class=" h1">Websocket</h1>
-
-<h3>
-    {#if ws}
-        Connected
-    {:else}
-        Not Connected
-    {/if}
-</h3>
-<br />
-<button
-    class=" border-2 m-2 px-1 rounded-md border-black"
-    on:click={() => subscribeToWs("ludo")}
->
-    Sub to 'Ludo'
-</button>
-<button
-    class=" border-2 m-2 px-1 rounded-md border-black"
-    on:click={() => subscribeToWs("yatzy")}>Sub to 'yatzy'</button
->
-<button
-    class=" border-2 m-2 px-1 rounded-md border-black"
-    on:click={() => subscribeToWs("gemgo")}>Sub to 'gemgo'</button
->
-
-<form on:submit={sendMsg}>
-    <input type="text" name="msgInp" id="msgInp" bind:value={msg} />
-    <input type="submit" value="Send" />
-</form>
-
-<div id="messages">
-    {#each messages as message}
-        <p>{message}</p>
-    {/each}
-</div>
+{#if $loggedIn}
+    <Chat />
+{:else}
+    <input type="text" name="usernameInp" id="usernameInp" bind:value={username}>
+    <button on:click={() => $loggedIn = true}>Enter</button>
+{/if}
---
+++ src/routes/websocket/chat.svelte
@@ -0,0 +1,82 @@
+<script>
+    import { onMount } from "svelte";
+    let ws;
+    let msg = "";
+
+    let messages = [];
+    onMount(() => {
+        ws = new WebSocket("ws://localhost:8080", {
+			headers: {
+				username: "philza"
+			}
+		});
+
+        ws.onopen = () => {
+            console.log("connected");
+        };
+
+        ws.onmessage = (event) => {
+            console.log(event.data);
+            messages = [...messages, event.data];
+        };
+
+        ws.onclose = () => {
+            console.log("disconnected");
+        };
+    });
+
+    function sendMsg() {
+        let data = JSON.stringify({
+            type: "chatMsg",
+            channel: "ludo",
+            message: msg,
+        });
+
+        ws.send(data);
+    }
+
+    function subscribeToWs(topic) {
+        let data = JSON.stringify({
+            type: "subscribe",
+            topicName: topic,
+        })
+
+        ws.send(data);
+    }
+</script>
+
+<h1 class=" h1">Websocket</h1>
+
+<h3>
+    {#if ws}
+        Connected
+    {:else}
+        Not Connected
+    {/if}
+</h3>
+<br />
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("ludo")}
+>
+    Sub to 'Ludo'
+</button>
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("yatzy")}>Sub to 'yatzy'</button
+>
+<button
+    class=" border-2 m-2 px-1 rounded-md border-black"
+    on:click={() => subscribeToWs("gemgo")}>Sub to 'gemgo'</button
+>
+
+<form on:submit={sendMsg}>
+    <input type="text" name="msgInp" id="msgInp" bind:value={msg} />
+    <input type="submit" value="Send" />
+</form>
+
+<div id="messages">
+    {#each messages as message}
+        <p>{message}</p>
+    {/each}
+</div>
